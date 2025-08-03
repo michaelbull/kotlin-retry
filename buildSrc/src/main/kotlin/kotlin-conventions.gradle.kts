a78ec24a568732586668c16c28867e490b190ffd
Michael Bull <mikebull94@gmail.com>
2025-08-03 21:29:11 +0100

    Adopt gradle-maven-publish-plugin

--- buildSrc/src/main/kotlin/kotlin-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/kotlin-conventions.gradle.kts
@@ -1,6 +1,5 @@
-import org.jetbrains.kotlin.gradle.targets.js.dsl.ExperimentalWasmDsl
-import org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootExtension
-import org.jetbrains.kotlin.gradle.targets.js.npm.tasks.KotlinNpmInstallTask
+import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_1_8
 
 plugins {
     kotlin("multiplatform")
@@ -9,9 +8,15 @@ plugins {
 kotlin {
     explicitApi()
 
-    jvmToolchain(8)
+    compilerOptions {
+        optIn.add("kotlin.contracts.ExperimentalContracts")
+    }
 
-    jvm()
+    jvm {
+        compilerOptions {
+            jvmTarget.set(JVM_1_8)
+        }
+    }
 
     js(IR) {
         browser()
@@ -59,12 +64,6 @@ kotlin {
     watchosDeviceArm64()
 
     sourceSets {
-        all {
-            languageSettings.apply {
-                optIn("kotlin.contracts.ExperimentalContracts")
-            }
-        }
-
         commonTest {
             dependencies {
                 implementation(kotlin("test"))
@@ -89,14 +88,4 @@ tasks.withType<Jar> {
     from(rootDir.resolve("LICENSE")) {
         into("META-INF")
     }
-}
-
-/* https://youtrack.jetbrains.com/issue/KT-63014/Running-tests-with-wasmJs-in-1.9.20-requires-Chrome-Canary#focus=Comments-27-8321383.0-0 */
-rootProject.the<NodeJsRootExtension>().apply {
-    nodeVersion = "21.0.0-v8-canary202309143a48826a08"
-    nodeDownloadBaseUrl = "https://nodejs.org/download/v8-canary"
-}
-
-rootProject.tasks.withType<KotlinNpmInstallTask> {
-    args.add("--ignore-engines")
 }
--- buildSrc/src/main/kotlin/publish-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/publish-conventions.gradle.kts
@@ -1,108 +1,82 @@
+import com.vanniktech.maven.publish.JavadocJar
+import com.vanniktech.maven.publish.KotlinMultiplatform
+
 plugins {
-    `maven-publish`
-    signing
+    id("com.vanniktech.maven.publish")
 }
 
-publishing {
-    repositories {
-        maven {
-            if (project.version.toString().endsWith("SNAPSHOT")) {
-                setUrl("https://oss.sonatype.org/content/repositories/snapshots")
-            } else {
-                setUrl("https://oss.sonatype.org/service/local/staging/deploy/maven2")
-            }
+mavenPublishing {
+    publishToMavenCentral()
+    signAllPublications()
 
-            credentials {
-                val ossrhUsername: String? by project
-                val ossrhPassword: String? by project
+    configure(
+        KotlinMultiplatform(
+            javadocJar = JavadocJar.Empty(),
+            sourcesJar = true,
+        )
+    )
 
-                username = ossrhUsername
-                password = ossrhPassword
+    pom {
+        name.set(project.name)
+        description.set(project.description)
+        url.set("https://github.com/michaelbull/kotlin-retry")
+        inceptionYear.set("2019")
+
+        licenses {
+            license {
+                name.set("ISC License")
+                url.set("https://opensource.org/licenses/isc-license.txt")
             }
         }
-    }
 
-    publications.withType<MavenPublication> {
-        val targetName = this@withType.name
+        developers {
+            developer {
+                name.set("Michael Bull")
+                url.set("https://www.michael-bull.com")
+            }
+        }
 
-        artifact(tasks.register("${targetName}JavadocJar", Jar::class) {
-            group = LifecycleBasePlugin.BUILD_GROUP
-            description = "Assembles a jar archive containing the Javadoc API documentation of target '$targetName'."
-            archiveClassifier.set("javadoc")
-            archiveAppendix.set(targetName)
-        })
+        contributors {
+            contributor {
+                name.set("Nicolas Dermine")
+                url.set("https://github.com/nicoder")
+            }
 
-        pom {
-            name.set(project.name)
-            description.set(project.description)
+            contributor {
+                name.set("Thorsten Hake")
+                url.set("http://www.thorsten-hake.com/")
+            }
+
+            contributor {
+                name.set("gnefedev")
+                url.set("https://github.com/gnefedev")
+            }
+
+            contributor {
+                name.set("cherrydev")
+                url.set("https://github.com/cherrydev")
+            }
+
+            contributor {
+                name.set("Joose Fjällström")
+                url.set("https://github.com/jfjallstrom")
+            }
+        }
+
+        scm {
+            connection.set("scm:git:https://github.com/michaelbull/kotlin-retry")
+            developerConnection.set("scm:git:git@github.com:michaelbull/kotlin-retry.git")
             url.set("https://github.com/michaelbull/kotlin-retry")
-            inceptionYear.set("2019")
+        }
 
-            licenses {
-                license {
-                    name.set("ISC License")
-                    url.set("https://opensource.org/licenses/isc-license.txt")
-                }
-            }
+        issueManagement {
+            system.set("GitHub Issues")
+            url.set("https://github.com/michaelbull/kotlin-retry/issues")
+        }
 
-            developers {
-                developer {
-                    name.set("Michael Bull")
-                    url.set("https://www.michael-bull.com")
-                }
-            }
-
-            contributors {
-                contributor {
-                    name.set("Nicolas Dermine")
-                    url.set("https://github.com/nicoder")
-                }
-
-                contributor {
-                    name.set("Thorsten Hake")
-                    url.set("http://www.thorsten-hake.com/")
-                }
-
-                contributor {
-                    name.set("gnefedev")
-                    url.set("https://github.com/gnefedev")
-                }
-
-                contributor {
-                    name.set("cherrydev")
-                    url.set("https://github.com/cherrydev")
-                }
-
-                contributor {
-                    name.set("Joose Fjällström")
-                    url.set("https://github.com/jfjallstrom")
-                }
-            }
-
-            scm {
-                connection.set("scm:git:https://github.com/michaelbull/kotlin-retry")
-                developerConnection.set("scm:git:git@github.com:michaelbull/kotlin-retry.git")
-                url.set("https://github.com/michaelbull/kotlin-retry")
-            }
-
-            issueManagement {
-                system.set("GitHub Issues")
-                url.set("https://github.com/michaelbull/kotlin-retry/issues")
-            }
-
-            ciManagement {
-                system.set("GitHub Actions")
-                url.set("https://github.com/michaelbull/kotlin-retry/actions")
-            }
+        ciManagement {
+            system.set("GitHub Actions")
+            url.set("https://github.com/michaelbull/kotlin-retry/actions")
         }
     }
-}
-
-signing {
-    val signingKeyId: String? by project // must be the last 8 digits of the key
-    val signingKey: String? by project
-    val signingPassword: String? by project
-
-    useInMemoryPgpKeys(signingKeyId, signingKey, signingPassword)
-    sign(publishing.publications)
 }

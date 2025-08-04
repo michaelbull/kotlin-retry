9211a0c38541761d7365d565c5091dd0d92021a0
Michael Bull <mikebull94@gmail.com>
2025-08-04 12:04:38 +0100

    Opt-in to UnsafeResultErrorAccess in retry result

--- kotlin-retry-result/src/commonMain/kotlin/com/github/michaelbull/retry/result/Retry.kt
+++ kotlin-retry-result/src/commonMain/kotlin/com/github/michaelbull/retry/result/Retry.kt
@@ -2,6 +2,7 @@ package com.github.michaelbull.retry.result
 
 import com.github.michaelbull.result.Err
 import com.github.michaelbull.result.Result
+import com.github.michaelbull.result.annotation.UnsafeResultErrorAccess
 import com.github.michaelbull.result.asErr
 import com.github.michaelbull.retry.attempt.Attempt
 import com.github.michaelbull.retry.attempt.firstAttempt
@@ -17,6 +18,7 @@ import kotlin.contracts.contract
  * Calls the specified function [block] and returns its [Result], handling any [Err] returned from the [block] function
  * execution retrying the invocation according to [instructions][RetryInstruction] from the [policy].
  */
+@OptIn(UnsafeResultErrorAccess::class)
 public suspend inline fun <V, E> retry(policy: RetryPolicy<E>, block: () -> Result<V, E>): Result<V, E> {
     contract {
         callsInPlace(block, InvocationKind.AT_LEAST_ONCE)

4a8406857666f593c040d34d3a1c8ab08772c4fa
Michael Bull <mikebull94@gmail.com>
2025-12-15 22:48:57 +0000

    Reorganise native platforms according to support tiers

--- buildSrc/src/main/kotlin/kotlin-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/kotlin-conventions.gradle.kts
@@ -27,26 +27,23 @@ kotlin {
 
     /* https://kotlinlang.org/docs/native-target-support.html#tier-1 */
 
-    macosX64()
     macosArm64()
     iosSimulatorArm64()
-    iosX64()
+    iosArm64()
 
     /* https://kotlinlang.org/docs/native-target-support.html#tier-2 */
 
     linuxX64()
     linuxArm64()
-
+    macosX64()
+    iosX64()
     watchosSimulatorArm64()
     watchosX64()
     watchosArm32()
     watchosArm64()
-
     tvosSimulatorArm64()
     tvosX64()
     tvosArm64()
-
-    iosArm64()
 
     /* https://kotlinlang.org/docs/native-target-support.html#tier-3 */
 
@@ -54,9 +51,7 @@ kotlin {
     androidNativeArm64()
     androidNativeX86()
     androidNativeX64()
-
     mingwX64()
-
     watchosDeviceArm64()
 
     sourceSets {
